810aa2e82d14bf0bb68fd7b1678c6b2eb1d5be2a
Александр Ульяницкий <a.ulyanitsky@gmail.com>
2011-01-05 18:32:08 +0200

    Правки верстки

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,9 @@
 module ApplicationHelper
+  # Set title
+  #
+  #   - title @page.title
+  def title(text, &block)
+    title = text.to_s || (yield if block_given?)
+    @page_title = @title = strip_tags(title)
+  end
 end
