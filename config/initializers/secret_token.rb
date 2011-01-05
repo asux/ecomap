da7d6ea9fe3d18d37aedd5e65579b94bab6369b8
Александр Ульяницкий <a.ulyanitsky@gmail.com>
2011-01-05 03:15:27 +0200

    Создан проект Ecomap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ecomap::Application.config.secret_token = '4c3463476f0fcf8069d8e588de57a5eb7d10aaaa99ae3149a190b076f450576a80dbb0864939820dac140a82b26479eb6be0f16c4b896e8cf33399ce43c5f770'
