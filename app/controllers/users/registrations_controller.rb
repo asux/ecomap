bb887cfe53d8de394fb41ea566612092e003cc54
Александр Ульяницкий <a.ulyanitsky@gmail.com>
2011-01-07 18:04:24 +0200

    Some fixes

--- app/controllers/users/registrations_controller.rb
+++ app/controllers/users/registrations_controller.rb
@@ -1,5 +1,5 @@
 class Users::RegistrationsController < Devise::RegistrationsController
-  before_filter :check_permissions, :only => [:new, :create, :cancel, :edit]
+  before_filter :check_permissions, :only => [:new, :create, :cancel, :edit, :update]
   skip_before_filter :require_no_authentication
        
   def check_permissions
