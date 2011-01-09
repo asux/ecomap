53bc0c5eeb982b02b94573f79d243cbdf4ad97a7
Александр Ульяницкий <a.ulyanitsky@gmail.com>
2011-01-10 01:58:30 +0200

    Changed production and staging hosts

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -47,5 +47,5 @@ Ecomap::Application.configure do
   # Send deprecation notices to registered listeners
   config.active_support.deprecation = :notify
   
-  config.action_mailer.default_url_options = { :host => 'ecomap.dikins.org.ua' }
+  config.action_mailer.default_url_options = { :host => 'ecomap.heroku.com' }
 end
